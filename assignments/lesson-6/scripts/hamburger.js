7e49103e93b791474c1bdef161ef0954b382fec4
lukelucas001 <luc18010@byui.edu>
2019-02-18 22:18:49 -0700

    apply week 7 pre changes

--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,4 +1,4 @@
-<script>
+
         /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
         function myFunction() {
             var x = document.getElementById("myTopnav");
@@ -9,4 +9,3 @@
                 x.className = "topnav";
             }
         }
-    </script>
